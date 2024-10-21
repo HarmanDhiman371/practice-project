62264d90d39839a63e899ffd24806e4772148a90
harman <harmandhiman6776@gmail.com>
2024-10-21 11:50:48 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-console.log("THIS")
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require(".middleware/errorHandler");
+const cors= require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+ 
+app.use(express.json());
+app.use(core());
+app.get('/' , (req , res)=>{
+    res.send("working");
+})
+app.listen(port , ()=>{
+    console.log(`server running on http://localhost:${port}`);
+})
