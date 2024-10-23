274f8f3eca93c516a1e3fe3ec67f136cc771d519
harman <harmandhiman6776@gmail.com>
2024-10-23 11:33:36 +0530

    hbs template

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middlewares/errorHandler");
 const cors= require("cors");
 
-
+const users = [
+    { name: "Harman Dhiman", age: 20 },
+    { name: "Hindveer", age: 19 },
+    { name: "Jaikirat", age: 20 },
+];
 const app = express();
 const port = process.env.PORT || 5000;
  const dotenv = require("dotenv");
@@ -14,6 +18,18 @@ app.use(cors());
 app.get('/' , (req , res)=>{
     res.send("working");
 })
+app.set('view engine' , 'hbs');
+app.get("/home",(req , res)=>{
+    res.render("home" , {
+       username:" Harman Dhiman",
+       posts : " time pass"
+    })
+})
+app.get("/alluser", (req, res) => {
+    res.render("alluser", {
+        users: users, 
+    });
+});
 app.listen(port , ()=>{
     console.log(`server running on http://localhost:${port}`);
 })
