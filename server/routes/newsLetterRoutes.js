fce1ec6695cfbddd03851282b6f04ea1884eabc3
harman <harmandhiman6776@gmail.com>
2024-11-14 18:35:36 +0530

    working of get post of newslletter

--- server/routes/newsLetterRoutes.js
+++ server/routes/newsLetterRoutes.js
@@ -1,7 +1,9 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
-const {getNewsletter , createNewsletter}=require("../controllers/newsletterController");
-const {jwtAuthMiddleware } = require("../middlewares/jwtMiddleware");
+const { getNewsletter, createNewsletter } = require("../controllers/newsletterController");
+const { validateJwtToken } = require("../middlewares/jwtMiddleware");
 
-router.get("/" , getNewsletter);
-routes.post("/" , createNewsletter , jwtAuthMiddleware);
+router.get("/", getNewsletter);
+router.post("/", validateJwtToken, createNewsletter); // Corrected order here
+
+module.exports = router;
